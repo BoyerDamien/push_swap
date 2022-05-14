26b14dd00c59e09d95796c65be0b59881a6b6b8f
dboyer <dboyer@student.42.fr>
2022-05-14 13:31:03 +0000

    fix: add int min max check

--- srcs/main.c
+++ srcs/main.c
@@ -1,30 +1,38 @@
 #include "push_swap.h"
 #include <stdlib.h>
+#include <limits.h>
+
+
+t_error *ft_check_max_min_int(int argc, char **argv) {
+    while (--argc) {
+        if (ft_atoi(argv[argc]) >= INT_MAX)
+            return ft_new_error("a number is greater than INT_MAX");
+        if (ft_atoi(argv[argc]) <= INT_MIN)
+            return ft_new_error("a number is less than INT_MIN");
+    }
+    return NULL;
+}
+
+void check_error(t_error *error) {
+    if (error)
+        {
+            error->show(error);
+            free(error);
+            exit(EXIT_FAILURE);
+        }
+}
 
 int main(int argc, char **argv)
 {
-    t_error *error;
-
     if (argc > 1) 
     {
-        error = ft_check_args(argc, argv);
-        if (error)
-        {
-            error->show(error);
-            free(error);
-            return 1;
-        }
+        check_error(ft_check_args(argc, argv));
+        check_error(ft_check_max_min_int(argc, argv));
 
-        error = ft_sort_stack(
+        check_error(ft_sort_stack(
                     ft_new_stack_with_args('a', argc, argv),
                     ft_new_stack('b')
-                );
-        if (error)
-        {
-            error->show(error);
-            free(error);
-            return 1;
-        }
+                ));
     }
     return 0;
 }
