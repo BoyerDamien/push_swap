278be491145e1bd698c40e6ddc36558505d8eab1
dboyer <dboyer@student.42.fr>
2022-05-14 13:39:09 +0000

    feat: operation encapsulation

--- srcs/ft_stack_reverse_rotate.c
+++ srcs/ft_stack_reverse_rotate.c
@@ -3,4 +3,7 @@
 void ft_stack_reverse_rotate(t_stack *self) 
 {
     self->list.first = self->list.last(&self->list);
+    ft_putstr("rr");
+    ft_putchar(self->stack_name);
+    ft_putchar('\n');
 }
