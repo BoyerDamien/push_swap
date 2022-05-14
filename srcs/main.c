b66387523bbfd2b2cc24bff0c33897b32e09b762
dboyer <dboyer@student.42.fr>
2022-05-14 13:33:20 +0000

    fix: wrong int mix and max logic test

--- srcs/main.c
+++ srcs/main.c
@@ -3,17 +3,17 @@
 #include <limits.h>
 
 
-t_error *ft_check_max_min_int(int argc, char **argv) {
+static t_error *ft_check_max_min_int(int argc, char **argv) {
     while (--argc) {
-        if (ft_atoi(argv[argc]) >= INT_MAX)
+        if (ft_atoi(argv[argc]) > INT_MAX)
             return ft_new_error("a number is greater than INT_MAX");
-        if (ft_atoi(argv[argc]) <= INT_MIN)
+        if (ft_atoi(argv[argc]) < INT_MIN)
             return ft_new_error("a number is less than INT_MIN");
     }
     return NULL;
 }
 
-void check_error(t_error *error) {
+static void ft_check_error(t_error *error) {
     if (error)
         {
             error->show(error);
@@ -26,10 +26,10 @@ int main(int argc, char **argv)
 {
     if (argc > 1) 
     {
-        check_error(ft_check_args(argc, argv));
-        check_error(ft_check_max_min_int(argc, argv));
+        ft_check_error(ft_check_args(argc, argv));
+        ft_check_error(ft_check_max_min_int(argc, argv));
 
-        check_error(ft_sort_stack(
+        ft_check_error(ft_sort_stack(
                     ft_new_stack_with_args('a', argc, argv),
                     ft_new_stack('b')
                 ));
